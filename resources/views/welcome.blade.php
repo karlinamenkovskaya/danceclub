d3c0cba7ac24cca94948270c51ee53f0e4ca7f61
karolinamenkovskaya <karolinamenkovskaya>
2018-10-05 12:14:49 +0300

    vsem privet

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,36 @@
+<!Doctype HTML>
+<html>
+   <head>
+<title> Танцевально-спортивный клуб "Триумф "</title>
+<meta charset="utf-8">
+<meta name="description" content="/ТСК ТРИУМФ ЛУЧШЕ ВСЕХ!!!!!!!">
+<meta name="keywords" content="бальные танцы,минск,тренеры,триумф">
+<link type="text/css" rel="stylesheet" href="media/bootstrap/css/bootstrap.min.css">   
+<link type="text/css" rel="stylesheet" href="media/css/style.css">   
+   </head>
+   <body>
+   <div class="header">
+   <img id="logo" src="/media/img/logo.png" alt="лол">
+   <h1 class="logotext">
+   Танцевально-спортивный клуб "ТРИУМФ"
+   </h1>
+   </div>
+   <nav class="topmenu">
+   <a href="#"> Новости </a>
+   <a href="#"> Главная </a>
+   <div class="container">
+   <div class="cd-md-2">
+   Меню
+   </div>
+      <div class="cd-md-8">
+	  <h2>Добро пожаловать на сайт</h2>
+	  текст
+	  <div class="cd-md-2">
+	  Виджеты
+	  </div>
+	  </div>
+
+   </body>
+   </nav>
+
+</html>
